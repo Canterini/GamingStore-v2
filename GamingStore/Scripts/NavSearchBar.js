1f7f6ea467a413241befc74129b81dc71e1de693
Franco Canterini <fcanterini@gmail.com>
2017-11-30 03:06:01 -0300

    Initial commit

---
+++ GamingStore/Scripts/NavSearchBar.js
@@ -0,0 +1,28 @@
+﻿
+function openNav() {
+
+    document.getElementById("mySidenav").style.width = "20%";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.0)";
+
+}
+
+
+function closeNav() {
+
+        var btn = document.getElementById("closebtn");
+        var barra = document.getElementById("mySidenav").style.width = "0px";
+
+    }
+
+
+
+
+
+
+
+
+
+   
+
+
+
